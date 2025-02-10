f502e92a8333d6ffa88d562eddf03af2f5358663
Otabek Abdusattorov <abdusattorovotabek19@gmail.com>
2025-02-10 15:11:51 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"GOlang/controllers"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	staticC := controllers.NewStatic()
+	usersC := controllers.NewUsers()
+
+	r := mux.NewRouter()
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		return
+	}
+}
