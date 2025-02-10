2545413ad7a9e170c43c2b6387e41d7246c620d9
Otabek Abdusattorov <abdusattorovotabek19@gmail.com>
2025-02-10 15:54:13 +0900

    updated user db

--- controllers/users.go
+++ controllers/users.go
@@ -1,24 +1,28 @@
 package controllers
 
 import (
+	"GOlang/models"
 	"GOlang/views"
 	"fmt"
 	"net/http"
 )
 
 type SignupForm struct {
+	Name     string `schema:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
 
-func NewUsers() *Users {
-	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-	}
-}
-
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
+}
+
+func NewUsers(us *models.UserService) *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
+	}
 }
 
 // New Get /signup
@@ -33,6 +37,7 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
+	fmt.Println(w, "Name is", form.Name)
 	fmt.Fprintln(w, "Email is", form.Email)
 	fmt.Fprintln(w, "Password is", form.Password)
 }
--- main.go
+++ main.go
@@ -2,20 +2,41 @@ package main
 
 import (
 	"GOlang/controllers"
+	"GOlang/models"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1903"
+	dbname   = "postgres"
+)
+
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
-	err := http.ListenAndServe(":3000", r)
+	err = http.ListenAndServe(":3000", r)
 	if err != nil {
 		return
 	}
