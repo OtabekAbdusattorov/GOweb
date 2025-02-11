aaa8da8a946dff7cacdcc7d000ab72f69f416f1c
unknown <abdusattorovotabek19@gmail.com>
2025-02-11 17:44:49 +0900

    Added authentification and stored hashpassword

--- models/users.go
+++ models/users.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -21,12 +22,19 @@ var (
 	// ErrInvalidID is returned when an invalid ID is provided
 	// to a method like Delete.
 	ErrInvalidID = errors.New("models: ID provided was invalid")
+	// Password pepper
+	userPwdPepper = "ThisIsElonMusk"
+	// ErrInvalidPassword is returned when an invalid password
+	// is used when attempting to authenticate a user.
+	ErrInvalidPassword = errors.New("models: incorrect password provided")
 )
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
 
 type UserService struct {
@@ -46,6 +54,13 @@ func NewUserService(connectionInfo string) (*UserService, error) {
 }
 
 func (us *UserService) Create(user *User) error {
+	pwdBytes := []byte(user.Password + userPwdPepper)
+	hashedBytes, err := bcrypt.GenerateFromPassword(pwdBytes, bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
@@ -96,6 +111,25 @@ func (us *UserService) Delete(id uint) error {
 	}
 	user := User{Model: gorm.Model{ID: id}}
 	return us.db.Delete(&user).Error
+}
+
+func (us *UserService) Authenticate(email string, password string) (*User, error) {
+	foundUser, err := us.ByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	err = bcrypt.CompareHashAndPassword(
+		[]byte(foundUser.PasswordHash),
+		[]byte(password+userPwdPepper))
+
+	switch {
+	case err == nil:
+		return foundUser, nil
+	case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+		return nil, ErrInvalidPassword
+	default:
+		return nil, err
+	}
 }
 
 func (us *UserService) AutoMigrate() error {
